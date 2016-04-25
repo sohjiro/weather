d88cde5ae764e168937b95400c246851cacbde50
Sohjiro <felipe@makingdevs.com>
2016-04-25 17:30:32 -0500

    fetching response and parsing

--- lib/weather/current_obs.ex
+++ lib/weather/current_obs.ex
@@ -1,8 +1,21 @@
 defmodule Weather.CurrentObs do
 
-
   def fetch do
+    weather_url
+    |> HTTPoison.get
+    |> handle_response
   end
 
+  def weather_url do
+    "http://w1.weather.gov/xml/current_obs/KDTO.xml"
+  end
+
+  def handle_response({:ok, %{status_code: 200, body: body}}) do
+    {:ok, Weather.XMLParser.parse!(body)}
+  end
+
+  def handle_response({_, %{status_code: status, body: body}}) do
+    {:ok, Weather.XMLParser.parse!(body)}
+  end
 
 end
